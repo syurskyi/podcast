da6ee848ba2d802d06de4b21da38796399a570a2
Grey <sergejyurskyj@yahoo.com>
2019-03-31 19:13:25 -0700

    Added a couple function

--- program.py
+++ program.py
@@ -16,13 +16,20 @@ def show_header():
 
 
 def display_results():
-
     start = random.randint(90, 110)
-    end = random.randint(130, service.get_latest_show_id()+1)
+    end = random.randint(130, service.get_latest_show_id() + 1)
 
     for show_id in range(start, end):
         info = service.get_episode(show_id)
         print("{}. {}".format(info.show_id, info.title))
+
+
+def new_header():
+    pass
+
+
+def last_header():
+    pass
 
 
 if __name__ == '__main__':
